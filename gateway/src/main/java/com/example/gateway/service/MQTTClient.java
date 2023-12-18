2055ebbf6ece87537c9a6815d7b9d2314c6c74fa
huuphuc <phuchph@gimo.vn>
2023-12-18 17:23:40 +0700

    update: sending data from gateway to node

--- gateway/src/main/java/com/example/gateway/service/MQTTClient.java
+++ gateway/src/main/java/com/example/gateway/service/MQTTClient.java
@@ -4,7 +4,9 @@ import java.util.UUID;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,11 +14,21 @@ public class MQTTClient {
     private final String mqttAddress = "tcp://127.0.0.1:1883";
     private final String temperatureTopic = "temperature";
     private final String nodeStartTopic = "node_start";
+    private final String controlNodeTopic = "control_node";
     private IMqttClient mqttClient;
 
     public MQTTClient() throws MqttException {
         String clientId = UUID.randomUUID().toString();
         this.mqttClient = new MqttClient(mqttAddress, clientId, new MemoryPersistence());
         mqttClient.connect();
+
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void sendControlNodeMessage() throws MqttException {
+        Long randomNumber = (long) (Math.random() * 100 + 1);
+        String messageContent = randomNumber.toString();
+        MqttMessage message = new MqttMessage(messageContent.getBytes());
+        mqttClient.publish(controlNodeTopic, message);
     }
 }
--- node/src/main/java/org/example/Main.java
+++ node/src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
@@ -12,11 +11,34 @@ public class Main {
     private static final String MQTT_ADDRESS = "tcp://127.0.0.1:1883";
     private static final String TEMPERATURE_TOPIC = "temperature";
     private static final String NODE_START_TOPIC = "node_start";
+    private static final String CONTROL_NODE_TOPIC = "control_node";
+
     public static void main(String[] args) throws MqttException {
         String clientId = args[0];
-        MqttClient mqttClient = new MqttClient(MQTT_ADDRESS, clientId, new MemoryPersistence());
-        mqttClient.connect();
-        mqttClient.subscribe(TEMPERATURE_TOPIC, 1);
-        System.out.println("abc");
+        MqttClient client = new MqttClient(MQTT_ADDRESS, clientId, new MemoryPersistence());
+        client.connect();
+        client.setCallback(new MqttCallback() {
+            public void connectionLost(Throwable cause) {
+                System.out.println("connectionLost: " + cause.getMessage());
+            }
+
+            public void messageArrived(String topic, MqttMessage message) {
+                if (topic.equals(CONTROL_NODE_TOPIC)) {
+                    handleControlNodeTopic(message);
+                }
+            }
+
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                System.out.println("deliveryComplete---------" + token.isComplete());
+            }
+        });
+
+        client.subscribe(CONTROL_NODE_TOPIC, 1);
     }
+
+    private static void handleControlNodeTopic(MqttMessage message) {
+        System.out.println(new String(message.getPayload()));
+    }
+
+
 }
